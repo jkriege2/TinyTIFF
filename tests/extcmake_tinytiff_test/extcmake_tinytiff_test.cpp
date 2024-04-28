5f1f503778117df25c2f38bac4d06c7bdf45e94d
jkriege2 <jan@jkrieger.de>
2024-04-28 22:32:56 +0200

    fixed issue https://github.com/jkriege2/TinyTIFF/issues/13

--- tests/extcmake_tinytiff_test/extcmake_tinytiff_test.cpp
+++ tests/extcmake_tinytiff_test/extcmake_tinytiff_test.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 
 int main() {
-    TinyTIFFWriterFile* tif=TinyTIFFWriter_open("extcmake_tinytiff_test.tif", 8, 1, 32, 32);
+    TinyTIFFWriterFile* tif=TinyTIFFWriter_open("extcmake_tinytiff_test.tif", 8, 1, 32, 32, TinyTIFFWriter_Greyscale);
 	if (tif) {
 		uint8_t* data=(uint8_t*)calloc(32*32,sizeof(uint8_t));
         TinyTIFFWriter_writeImage(tif, data);
